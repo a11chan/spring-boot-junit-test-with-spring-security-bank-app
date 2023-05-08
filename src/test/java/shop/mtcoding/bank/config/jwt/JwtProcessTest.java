361d3a957d3846ad5c405d5178e786c89328a443
allchan <a11chan@hotmail.com>
2023-05-08 15:54:57 +0900

    4. Bank 애플리케이션 -  Jwt토큰 만료시간 버그 수정

--- src/test/java/shop/mtcoding/bank/config/jwt/JwtProcessTest.java
+++ src/test/java/shop/mtcoding/bank/config/jwt/JwtProcessTest.java
@@ -1,24 +1,25 @@
 package shop.mtcoding.bank.config.jwt;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import shop.mtcoding.bank.config.auth.LoginUser;
 import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserEnum;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class JwtProcessTest {
 
+    private String createToken() {
+        User user = User.builder().id(1L).role(UserEnum.ADMIN).build();
+        LoginUser loginUser1 = new LoginUser(user);
+
+        return JwtProcess.create(loginUser1);
+    }
+
     @Test
     public void create_test() throws Exception {
-        User user = User.builder()
-                .id(1L)
-                .role(UserEnum.CUSTOMER)
-                .build();
-        LoginUser loginUser = new LoginUser(user);
-
-        String jwtToken = JwtProcess.create(loginUser);
+        String jwtToken = createToken();
         System.out.println("jwtToken = " + jwtToken);
 
         assertTrue(jwtToken.startsWith(JwtVO.TOKEN_PREFIX));
@@ -27,13 +28,16 @@ class JwtProcessTest {
     @Test
     void verify_test() throws Exception {
         //given
-        String jwtToken = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiYW5rIiwiZXhwIjoxNjgzNzg2Nzc1LCJpZCI6MSwicm9sZSI6IkNVU1RPTUVSIn0.8mya6pFtar73LY80wNvLL9LJmvosqgrxg9yiYS55pW0KguRJXpQCppSN1Tz9jnyUHS2QOCPbDYOB4HKraoWJ1w";
+        String token = createToken();
+        String jwtToken = token.replace(JwtVO.TOKEN_PREFIX, "");
 
         //when
-        LoginUser loginUser = JwtProcess.verify(jwtToken);
-        System.out.println("loginUser = " + loginUser.getUser().getId());
+        LoginUser loginUser2 = JwtProcess.verify(jwtToken);
+        System.out.println("loginUser2 = " + loginUser2.getUser().getId());
+        System.out.println("loginUser2 = " + loginUser2.getUser().getRole().name());
 
         //then
-        Assertions.assertThat(loginUser.getUser().getId()).isEqualTo(1L);
+        assertThat(loginUser2.getUser().getId()).isEqualTo(1L);
+        assertThat(loginUser2.getUser().getRole()).isEqualTo(UserEnum.ADMIN);
     }
 }
